63d5acdedd3a9580078c9a21cc6e63976ee7299d
ulasanekonomi-collab <ulasanekonomi@gmail.com>
2026-05-11 06:35:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ with col1:
 
 with col2:
     st.title("PIOM Institutional Design Lab")
-    st.caption("Laboratorium desain kelembagaan untuk diagnosis, simulasi, refleksi, dan rekayasa institusional.")
+    st.caption("Dikembangkan oleh Yuhka Sundaya, Ekonomi Pembangunan Unisba")
 
 st.image(foto, width=180)
 st.markdown("""
